f12cd36f050a2db1bb6f365e72a73500e63501e9
Krisanov <holapa10@mail.ru>
2018-11-19 16:00:42 +0300

    Добавьте файлы проекта.

---
+++ 2лабораторная/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace _2лабораторная
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.StartMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.StopMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartMenu,
+            this.StopMenu});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // StartMenu
+            // 
+            resources.ApplyResources(this.StartMenu, "StartMenu");
+            this.StartMenu.Name = "StartMenu";
+            this.StartMenu.Click += new System.EventHandler(this.StartMenu_Click);
+            // 
+            // StopMenu
+            // 
+            resources.ApplyResources(this.StopMenu, "StopMenu");
+            this.StopMenu.Name = "StopMenu";
+            this.StopMenu.Click += new System.EventHandler(this.StopMenu_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem StartMenu;
+        private System.Windows.Forms.ToolStripMenuItem StopMenu;
+    }
+}
+
---
+++ 2лабораторная/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2лабораторная
+{
+    public partial class Form1 : Form
+    {
+        delegate void D_Time();
+
+        event D_Time SecondTime;
+
+        public Form1()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("En-en");
+
+            InitializeComponent();
+            SecondTime += Show_Time_TextBox;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SecondTime();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && e.Alt)
+                StartMenu.PerformClick();
+
+            if (e.KeyCode == Keys.Left && e.Alt)
+                StopMenu.PerformClick();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            KeyPreview = true;
+        }
+
+        private void Show_Time_TextBox()
+        {
+            Clock kik = new Clock();
+
+            textBox1.Text = kik.tim("Moscow").ToString("dd/MM/yyyy H:mm:ss "); ;
+            textBox2.Text = kik.tim("London").ToString("dd/MM/yyyy H:mm:ss "); ;
+            textBox3.Text = kik.tim("Vladivostok").ToString("dd/MM/yyyy H:mm:ss "); ;
+        }
+
+        private void StartMenu_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void StopMenu_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+
+    class Clock
+    {
+        public DateTime tim(string name)
+        {
+            Dictionary<string, TimeSpan> slovar = new Dictionary<string, TimeSpan>
+            {
+                {"Moscow",new TimeSpan(0, 0, 0)},
+                {"London",new TimeSpan(-2, 0, 0)},
+                {"Vladivostok",new TimeSpan(7, 0, 0)}
+            };
+
+            DateTime now = DateTime.Now;
+            now += slovar[name];
+
+            return now;
+        }
+    }
+}
